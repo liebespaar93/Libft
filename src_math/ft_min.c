5f2cc27315dfaa52169a56f3e6e2152e28bcf987
MacBook Air <liebespaar93@gmail.com>
2022-09-13 07:24:19 +0900

    2022/09/13 min fix

--- src_math/ft_min.c
+++ src_math/ft_min.c
@@ -6,7 +6,7 @@
 /*   By: kyoulee <kyoulee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 15:11:45 by kyoulee           #+#    #+#             */
-/*   Updated: 2022/09/02 17:19:07 by kyoulee          ###   ########.fr       */
+/*   Updated: 2022/09/08 03:30:32 by kyoulee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_min(int num1, int num2)
 
 float	ft_float_min(float num1, float num2)
 {
-	if (num1 > num2)
+	if (num1 < num2)
 		return (num1);
 	return (num2);
 }
