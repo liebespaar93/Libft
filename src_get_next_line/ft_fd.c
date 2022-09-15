bdf0833333df1010355b6f8be233abaf0c9c654a
MacBook Air <liebespaar93@gmail.com>
2022-09-15 12:58:58 +0900

    2022/08/15 ft_fd fix \n

--- src_get_next_line/ft_fd.c
+++ src_get_next_line/ft_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_fd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kyoulee <kyoulee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: kyoulee <kyoulee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 14:45:30 by kyoulee           #+#    #+#             */
-/*   Updated: 2022/08/01 14:45:33 by kyoulee          ###   ########.fr       */
+/*   Updated: 2022/09/15 12:56:36 by kyoulee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_open(char *filename, int option)
 	int		fd;
 
 	fd = open(filename, option);
-	if (fd < 0 && write(2, "fd_error\n", 8))
+	if (fd < 0 && write(2, "fd_error\n", 9))
 		exit(1);
 	return (fd);
 }
